c28fde9b8c709e07c088aacc5818c45973caf9ac
menn1s <dennisqvt@protonmail.com>
2023-12-12 14:57:32 -0800

    added regex support

--- docx-searcher.py
+++ docx-searcher.py
@@ -2,6 +2,7 @@ from colorama import Fore
 import os
 import docx
 import argparse
+import re
 
 def search_keywords_in_docx_insensitive(file_path, keywords):
     doc = docx.Document(file_path)
@@ -19,6 +20,21 @@ def search_keywords_in_docx_insensitive(file_path, keywords):
                     print("\t1. Found under heading: " + doc.paragraphs[i-k].text)
                 print("\t2. Matched Paragraph: " + doc.paragraphs[i].text)
 
+def search_keywords_in_docx_regex(file_path, keywords):
+    doc = docx.Document(file_path)
+    for i, paragraph in enumerate(doc.paragraphs):
+        for keyword in keywords:
+            pattern = re.compile(keyword, re.IGNORECASE)
+            if pattern.search(paragraph.text):
+                print("- Found some matches in " + file_path)
+                # set starting offset to iterate upwards and find the heading containing the matched text
+                k = 1
+                while not doc.paragraphs[i - k].style.name.startswith('Heading'):
+                    k += 1
+                if doc.paragraphs[i - k].style.name.startswith('Heading'):
+                    print("\t1. Found under heading: " + doc.paragraphs[i - k].text)
+                print("\t2. Matched Paragraph: " + doc.paragraphs[i].text)
+
 def search_keywords_in_docx(file_path, keywords):
     doc = docx.Document(file_path)
     for i, paragraph in enumerate(doc.paragraphs):
@@ -34,7 +50,7 @@ def search_keywords_in_docx(file_path, keywords):
                     print("\t1. Found under heading: " + doc.paragraphs[i-k].text)
                 print("\t2. Matched Paragraph: " + doc.paragraphs[i].text)
 
-def process_directory(directory_path, keywords, case_insensitive):
+def process_directory(directory_path, keywords, case_insensitive, extend_regex):
     for root, _, files in os.walk(directory_path):
         for file_name in files:
             if file_name.endswith('.docx') and not file_name.startswith('~'):
@@ -42,6 +58,13 @@ def process_directory(directory_path, keywords, case_insensitive):
                 if (case_insensitive):
                     try:
                         search_keywords_in_docx_insensitive(file_path, keywords)
+                    except:
+                        print("Check that the file is not corrupted or password protected: " + file_path)
+                        pass
+                        #print(f"Found keywords in: {file_path}")
+                elif (extend_regex):
+                    try:
+                        search_keywords_in_docx_regex(file_path, keywords)
                     except:
                         print("Check that the file is not corrupted or password protected: " + file_path)
                         pass
@@ -56,9 +79,13 @@ def process_directory(directory_path, keywords, case_insensitive):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Search for keywords in DOCX files in a directory.")
+    exclusive_group = parser.add_mutually_exclusive_group()
+    exclusive_group.add_argument("-i", "--case_insensitive", action="store_true", help="Search case insensitive")
+    exclusive_group.add_argument("-e", "--extend_regex", action="store_true", help="Search with regex string")
     parser.add_argument("directory_path", help="Path to the directory containing DOCX files.")
     parser.add_argument("keywords", nargs="+", help="Keywords to search for in the DOCX files.")
-    parser.add_argument("-i", "--case_insensitive", action="store_true", help="Search case insensitive")
+    #parser.add_argument("-i", "--case_insensitive", action="store_true", help="Search case insensitive")
+    #parser.add_argument("-e", "--extend_regex", action="store_true", help="Search with regex string")
     args = parser.parse_args()
 
-    process_directory(args.directory_path, args.keywords, args.case_insensitive)
+    process_directory(args.directory_path, args.keywords, args.case_insensitive, args.extend_regex)
