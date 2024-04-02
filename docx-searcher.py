fa5b0526cdbc89b1d2dab3e46ab7c4b43a474263
menn1s <dennisqvt@protonmail.com>
2024-04-02 13:17:49 -0700

    added colors! Also added a flag for heading search

--- docx-searcher.py
+++ docx-searcher.py
@@ -10,7 +10,7 @@ def search_keywords_in_docx_insensitive(file_path, keywords):
     for i, paragraph in enumerate(doc.paragraphs):
         for keyword in keywords:
             if keyword.lower() in paragraph.text.lower():
-                print("- Found some matches in " + file_path)
+                print(Fore.CYAN + "- Found some matches in " + Fore.YELLOW + file_path + Fore.RESET)
                 # set starting offset to iterate upwards and find the heading containing the matched text
                 k = 1
                 while not doc.paragraphs[i-k].style.name.startswith('Heading'):
@@ -26,7 +26,7 @@ def search_keywords_in_docx_regex(file_path, keywords):
         for keyword in keywords:
             pattern = re.compile(keyword, re.IGNORECASE)
             if pattern.search(paragraph.text):
-                print("- Found some matches in " + file_path)
+                print(Fore.Cyan + "- Found some matches in " + Fore.YELLOW + file_path + Fore.RESET)
                 # set starting offset to iterate upwards and find the heading containing the matched text
                 k = 1
                 while not doc.paragraphs[i - k].style.name.startswith('Heading'):
@@ -40,7 +40,7 @@ def search_keywords_in_docx(file_path, keywords):
     for i, paragraph in enumerate(doc.paragraphs):
         for keyword in keywords:
             if keyword in paragraph.text:
-                print("- Found some matches in " + file_path)
+                print(Fore.Cyan + "- Found some matches in " + Fore.YELLOW + file_path + Fore.RESET)
                 # set starting offset to iterate upwards and find the heading containing the matched text
                 k = 1
                 while not doc.paragraphs[i-k].style.name.startswith('Heading'):
@@ -82,6 +82,7 @@ if __name__ == "__main__":
     exclusive_group = parser.add_mutually_exclusive_group()
     exclusive_group.add_argument("-i", "--case_insensitive", action="store_true", help="Search case insensitive")
     exclusive_group.add_argument("-e", "--extend_regex", action="store_true", help="Search with regex string")
+    parser.add_argument("-H", "--header", action="store_true", help="Search for matching headers only")
     parser.add_argument("directory_path", help="Path to the directory containing DOCX files.")
     parser.add_argument("keywords", nargs="+", help="Keywords to search for in the DOCX files.")
     #parser.add_argument("-i", "--case_insensitive", action="store_true", help="Search case insensitive")
